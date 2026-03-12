33562a090a5582957e052d61e61ffe2828e7b81c
see-ann <seanshaochenwang@gmail.com>
2026-03-11 23:47:18 -0400

    feat: update configuration and add Biome integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      }
-    })
-  ]
+	integrations: [
+		react({
+			babel: {
+				plugins: ["babel-plugin-react-compiler"],
+			},
+		}),
+	],
 });
