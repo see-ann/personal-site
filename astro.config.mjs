4f8df00ff00706cae80579cd2db2c28e17940fca
see-ann <seanshaochenwang@gmail.com>
2026-03-11 23:45:12 -0400

    feat: add babel-plugin-react-compiler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      }
+    })
+  ]
 });
